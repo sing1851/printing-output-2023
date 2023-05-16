c906a11aa83b0c56dc279fedd52a090c1aa6c7bc
sing1851 <132909434+sing1851@users.noreply.github.com>
2023-05-15 22:39:16 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-
 //Task 1
 
 let a = 3;
@@ -7,19 +6,32 @@ let c;
 
 // alert( "description of alerted_value: " + alerted_value );
 
-let output = `
+let print = `
 
     let a = 3;
     let b = 5;
     let c;
     ............
-    a + b = ${ a + b }
-    a - b = ${ a - b }
-    
+    a+b= ${a+b}
+    a - b = ${a - b}
+    a * b = ${a * b}
+    a / b = ${a / b}
+    a % b = ${a % b}
+    a += b = ${a += b}
+    a -= b = ${a -= b}
+    a *= b = ${a*= b}
+    a /= b = ${a/= b}
+    a %= b = ${a %= b}
+    a == b = ${a == b}
+    a != b = ${a != b}
+    a > b = ${a > b}
+    a < b = ${a < b}
+    !a && !c = ${!a && !c}
+    !a || !c = ${!a || !c}
     
     `;
-    alert(output);
-    
+    alert(print);
+
 /*
 a + b
 a - b
@@ -38,3 +50,12 @@ a < b
 !a && !c 
 !a || !c 
 */
+
+//Task2
+
+let first_name= "Tarandeep";
+let last_name= "Singh";
+let email= "sing1851@algonquinlive.com.com";
+let output= `My name is ${first_name} ${last_name}. You can contact me at ${email}.`;
+
+alert(output);
